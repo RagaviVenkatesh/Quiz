ca42f9d5f7e5af7b079683384bbd307e980405f7
RagaviVenkatesh <ragavivenkatesh2121@gmail.com>
2020-06-25 21:49:48 +0530

    first commit

---
+++ js/start.js
@@ -0,0 +1,31 @@
+var objPeople = [
+  { 
+    username: "test1",
+    password: "test1"
+  },
+  { 
+    username: "test2",
+    password: "test2"
+  }
+
+]
+
+
+function submitForm(e) {
+    e.preventDefault();
+    let name = document.forms["welcome_form"]["name"].value;
+  var username = document.getElementById('username').value
+  var password = document.getElementById('password').value
+  for (var i = 0; i < objPeople.length; i++) 
+  {
+    if (username == objPeople[i].username && password == objPeople[i].password) 
+    {
+      console.log(username + " is logged in!!!")
+      sessionStorage.setItem("name", name);
+      location.href = "quiz.html";    }
+  }
+  document.getElementById('error').innerHTML ='Invalid Credentials';
+  console.log("Incorrect username or password")
+  
+   
+  }
